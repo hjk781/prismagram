f09ceaea80a064c811d92a34e7343e62d3da3a56
yeonggwan <hjk781@naver.com>
2019-05-03 10:08:27 +0900

    toggleLike Resolver part One

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -3,7 +3,8 @@ import { generateTorken } from "../../../utils";
 
 export default {
   Mutation: {
-    confirmSecret: async (_, args) => {
+    confirmSecret: async (_, args, { request }) => {
+      console.log(request);
       const { email, secret } = args;
       const user = await prisma.user({ email });
       if (user.loginSecret === secret) {
